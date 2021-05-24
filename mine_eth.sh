08d9c73bc2967d1921bb58c29f27aa759c20226e
andreas5555 <49350051+andreas5555@users.noreply.github.com>
2021-05-24 20:35:38 +0300

    Update mine_eth.sh

--- mine_eth.sh
+++ mine_eth.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DHxKgzLZ9g9Er85UXbW39stpRpjG55LHRp.GGL $@
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DHdVQ2UrKVTgm6x6kd1RxYjvH1cBebJoGv.GGL#55yv-sfm8 $@
